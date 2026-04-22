3ec13835d754a5389ebf8d0062d4df9f484529cc
Ben Moreau <bigben.mro@gmail.com>
2026-04-22 15:28:25 -0300

    Corrección ortográfica en el comentario de la función multiplicar

--- operaciones.c
+++ operaciones.c
@@ -13,7 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-// La función [multiplicar] evalúa dos parámetros de tipo entero y retorna el producto de los mismos
+// La función [multiplicar] evalúa dos parámetros de tipo entero y retorna el producto de los mismos.
 int multiplicar(int a, int b) {
     return a*b; /* TODO: implementar */
 }
